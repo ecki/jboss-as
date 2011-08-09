0c5f8851aaf25fc1ea312b00f446ff254d311418
ecki <bernd@eckenfels.net>
2011-08-09 20:45:42 +0300

    run.bat does not exist anymore.

--- build/src/main/resources/bin/domain.conf.bat
+++ build/src/main/resources/bin/domain.conf.bat
@@ -4,12 +4,12 @@ rem #  JBoss Bootstrap Script Configuration                                    #
 rem #                                                                          ##
 rem #############################################################################
 
-rem # $Id: run.conf.bat 88820 2009-05-13 15:25:44Z dimitris@jboss.org $
+rem # $Id: $
 
 rem #
-rem # This batch file is executed by run.bat to initialize the environment
-rem # variables that run.bat uses. It is recommended to use this file to
-rem # configure these variables, rather than modifying run.bat itself.
+rem # This batch file is executed by domain.bat to initialize the environment 
+rem # variables that domain.bat uses. It is recommended to use this file to
+rem # configure these variables, rather than modifying domain.bat itself.
 rem #
 
 if not "x%JAVA_OPTS%" == "x" (
@@ -22,20 +22,20 @@ rem # Specify the JBoss Profiler configuration file to load.
 rem #
 rem # Default is to not load a JBoss Profiler configuration file.
 rem #
-rem set "PROFILER=%JBOSS_HOME%\bin\jboss-profiler.properties"
+rem set PROFILER="%JBOSS_HOME%\bin\jboss-profiler.properties"
 
 rem #
 rem # Specify the location of the Java home directory (it is recommended that
 rem # this always be set). If set, then "%JAVA_HOME%\bin\java" will be used as
 rem # the Java VM executable; otherwise, "%JAVA%" will be used (see below).
 rem #
-rem set "JAVA_HOME=C:\opt\jdk1.6.0_23"
+rem set JAVA_HOME="C:\opt\jdk1.6.0_23"
 
 rem #
 rem # Specify the exact Java VM executable to use - only used if JAVA_HOME is
 rem # not set. Default is "java".
 rem #
-rem set "JAVA=C:\opt\jdk1.6.0_23\bin\java"
+rem set JAVA="C:\opt\jdk1.6.0_23\bin\java"
 
 rem #
 rem # Specify options to pass to the Java VM. Note, there are some additional
@@ -43,17 +43,17 @@ rem # options that are always passed by run.bat.
 rem #
 
 rem # JVM memory allocation pool parameters - modify as appropriate.
-set "JAVA_OPTS=-Xms64M -Xmx512M -XX:MaxPermSize=256M"
+set JAVA_OPTS=-Xms64M -Xmx512M -XX:MaxPermSize=256M
 
 rem # Prefer IPv4
 set "JAVA_OPTS=%JAVA_OPTS% -Djava.net.preferIPv4Stack=true"
 
 rem # Warn when resolving remote XML DTDs or schemas.
-set "JAVA_OPTS=%JAVA_OPTS% -Dorg.jboss.resolver.warning=true"
+set JAVA_OPTS=%JAVA_OPTS% -Dorg.jboss.resolver.warning=true
 
 rem # Make Byteman classes visible in all module loaders
 rem # This is necessary to inject Byteman rules into AS7 deployments
-set "JAVA_OPTS=%JAVA_OPTS% -Djboss.modules.system.pkgs=org.jboss.byteman"
+set JAVA_OPTS=%JAVA_OPTS% -Djboss.modules.system.pkgs=org.jboss.byteman
 
 rem # Set the default configuration files to use if -c, --domain-config or --host-config are not used
 set "JAVA_OPTS=%JAVA_OPTS% -Djboss.domain.default.config=domain.xml -Djboss.host.default.config=host.xml"
@@ -76,4 +76,3 @@ rem set "PROCESS_CONTROLLER_JAVA_OPTS=%PROCESS_CONTROLLER_JAVA_OPTS% -agentlib:j
 rem set "HOST_CONTROLLER_JAVA_OPTS=%HOST_CONTROLLER_JAVA_OPTS% -agentlib:jdwp=transport=dt_shmem,address=jboss,server=y,suspend=n"
 
 :JAVA_OPTS_SET
-
